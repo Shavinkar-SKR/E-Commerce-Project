05ed0ed0d2ed60b0311a6676550a5872244f7257
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-06-19 22:02:47 +0530

    Created controller and route for Product
    Tested the functionality with Postman with the endpoint http://localhost:8000/api/v1/products to see the results.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require("express"); //imports the express package
 const app = express(); //creates an instance of the express
+const productsRouter = require("./routes/productsRoute");
+
+app.use("/api/v1", productsRouter);
 
 module.exports = app; //app object is set to export to other files
