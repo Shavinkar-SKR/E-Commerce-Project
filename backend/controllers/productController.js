0ae4b5aa5e1aeea4cbb96a38942b87f0a07b591d
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-06-23 15:27:05 +0530

    Created the Update Operation
    This help to find a product by id, run validations defined in the productModel.js and updates property/s of a product by returning with updated data (new:true)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,14 +22,40 @@ exports.newProduct = async (req, res, next) => {
 
 //Get One Product - api/v1/product/:id
 exports.getOneProduct = async (req, res, next) => {
-  const product = await productModel.findById(req.params.id);
+  const product = await productModel.findById(req.params.id); //searches a product based on the id passed in the url
 
   if (!product) {
+    //if proudct is null then it shows a 404 error saying product not found
     res.status(404).json({
       success: false,
       message: "Product not found",
     });
   }
+
+  res.status(200).json({
+    //if product is found with that id then it shows 200 success response
+    success: true,
+    product,
+  });
+};
+
+//Update Product - /api/v1/product/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await productModel.findById(req.params.id);
+
+  if (!product) {
+    res.status(404).json({
+      success: false,
+      message: "Product not found!!!!",
+    });
+  }
+
+  product = await productModel.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true, //ensures that Mongoose validation rules (like required, maxLength, enum, etc.) defined in the schema are enforced during the update.
+
+    new: true, //it gives the updated product data instead of the old product data,
+    // eg: if stock is updated to 10 where original value was 5, then it should show the all the properties of the product with the updated one
+  });
 
   res.status(200).json({
     success: true,
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -3,11 +3,13 @@ const {
   getProducts,
   newProduct,
   getOneProduct,
+  updateProduct,
 } = require("../controllers/productController");
 const router = express.Router();
 
 router.route("/products").get(getProducts);
 router.route("/product/new").post(newProduct); //for this url if post request is triggered then newProduct handler function is called
 router.route("/product/:id").get(getOneProduct);
+router.route("/product/:id").put(updateProduct);
 
 module.exports = router;
