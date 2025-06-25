dbfd3ba32176cee40258d2d8f54d4bf4c434801b
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-06-25 21:53:27 +0530

    Initiated Unhandled Rejection Error
    Check comments for better understanding.

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,20 @@ dotenv.config({ path: path.join(__dirname, "config/.env") }); //path usesa metho
 
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   //.listen creates a http server using express module
   console.log(
     `Server is running on Port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
   );
+});
+
+//Handling Unhandled rejection error - this happens if Promise errors are not caught
+//process is a global variable of Node, on() is an eventlistener.
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled rejection error`);
+  server.close(() => {
+    //with server.close() only server is getting closed but node program is still running, so....
+    process.exit(1); //to stop node program added a callback function stop using exit()
+  });
 });
