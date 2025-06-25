99dc699480b385b91c42ec664df4d5a731a8e102
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-06-25 21:56:23 +0530

    Modified the code for a smooth run
    To test the unhandled rejection error, uri and catch block in db.js was modified. So brought back to earlier version

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,11 @@ const connectDatabase = () => {
     .connect(process.env.DB_URI) //connects the mongodb locally with the uri in the .env file
     .then((con) => {
       console.log(`Connected to MongoDB on host: ${con.connection.host}`); //prints a message with the connection host
+    })
+    //If there is no catch block, it shows uncaught rejection error - it cannot happen for invalid url...
+    .catch((err) => {
+      console.log(err.message);
     });
-  //If there is no catch block, it shows uncaught rejection error - it cannot happen for invalid url...
-  // .catch((err) => {
-  //   console.log(err.message);
-  // });
 };
 
 module.exports = connectDatabase;
