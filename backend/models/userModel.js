7b7e778c7660270e3abf13e3a7cf5223a4de4d80
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-03 19:48:23 +0530

    Initiated the User Model
    This gives the blueprint for a user. Added the 'validator' package to dependencies for email validation.

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter the name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter the email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter the password"],
+    maxlength: [6, "Password cannot exceed 6 characters"],
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordTokenExpire: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+let userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
