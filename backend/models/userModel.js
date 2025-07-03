37e83b318d6dbe302cfee4498d7e96b2b3837acf
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-03 20:41:54 +0530

    Initiated Password hashing
    Installed bcrpyt module for hashing password. Hashed password is stored in the db.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,6 +37,10 @@ const userSchema = new mongoose.Schema({
     default: Date.now(),
   },
 });
+
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+}); //before saving a user document it hashes the password using bcrypt module
 
 let userModel = mongoose.model("User", userSchema);
 
