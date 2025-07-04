a70021247aabdc22d1a2a46e53cb3e87a1f3710e
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-04 18:40:30 +0530

    Added comments

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,14 +2,18 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const userModel = require("../models/userModel");
 
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  const { name, email, password, avatar } = req.body;
+  const { name, email, password, avatar } = req.body; //Destructure user details (name, email, password, avatar) from request body
 
+  //Create a new user document in MongoDB
   const user = await userModel.create({
     name,
     email,
     password,
     avatar,
   });
+  // user.password = undefined; //Before sending the user in the response, manually set the password to undefined:
+  //This does not delete the password from the database.
+  //It just removes it from the response object.
 
   res.status(201).json({
     success: true,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,13 +10,15 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please enter the email"],
-    unique: true,
-    validate: [validator.isEmail, "Please enter a valid email"],
+    unique: true, //Ensures no two users have the same email
+    validate: [validator.isEmail, "Please enter a valid email"], //Validates email format
   },
   password: {
     type: String,
     required: [true, "Please enter the password"],
     maxlength: [6, "Password cannot exceed 6 characters"],
+    //select: false, //It hides the field from being returned in queries like .find() or .findOne().
+    //const user = await User.findOne({ email }).select("+password");
   },
   avatar: {
     type: String,
@@ -39,7 +41,8 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function (next) {
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10); //It defines how many times the hashing algorithm is applied internally.
+  //means: hash it using a salt, and repeat the hashing process 2^10 = 1024 times
 }); //before saving a user document it hashes the password using bcrypt module
 
 let userModel = mongoose.model("User", userSchema);
