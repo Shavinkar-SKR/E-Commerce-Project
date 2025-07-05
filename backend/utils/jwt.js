5e2e9f05859b4d187c0ebba9b7f5f33ee9b2a036
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-05 20:46:10 +0530

    Developed a cookie sent via http
    Introduced cookie-based storage for JWT tokens with HTTP-only flag for enhanced security.

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -2,11 +2,26 @@ const generateToken = (user, statusCode, res) => {
   //Generating JWT Token
   const token = user.getJwtToken();
 
-  res.status(statusCode).json({
-    success: true,
-    token,
-    user,
-  });
+  //Cookie options for storing the token securely
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRY_TIME * 1000 * 60 * 60 * 24 //expiry is set to 7 days by converting it to milliseconds
+    ),
+    httpOnly: true, //This cookie will be accessible only via http, not JavaScript (for security)
+  };
+
+  res
+    .status(statusCode)
+    .cookie(
+      "token" /*This is a name of the key*/,
+      token /*This is the value of that key*/,
+      options
+    )
+    .json({
+      success: true,
+      token,
+      user,
+    });
 };
 
 module.exports = generateToken;
