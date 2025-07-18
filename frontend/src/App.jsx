4f689421887f270ac2f99f02cd892a8c9fb7e9d8
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-18 19:27:33 +0530

    Add Header and Footer components to App layout
    Introduced new Header and Footer components and integrated them into the main App component. Added a logo image for branding. The Header includes navigation, search, login, and cart UI elements, while the Footer provides copyright.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
 
 function App() {
   return (
-    <>
-      <div>App Component</div>
-    </>
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
