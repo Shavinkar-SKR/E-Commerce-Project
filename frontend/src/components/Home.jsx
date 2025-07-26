cac6003948ca82eef3b733bb9999a5949798c3ca
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-26 12:38:57 +0530

    Installed Product component
    Extracted the product card markup from Home.jsx into a new Product.jsx component for better modularity and code reuse. Updated Home.jsx to use the new Product component when rendering the list of products.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,7 @@ import MetaData from "./layouts/MetaData"; //customed function imported to pass 
 import { getProducts } from "../actions/productsAction";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "./layouts/Loader";
+import Product from "./product/Product";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -25,36 +26,7 @@ export default function Home() {
             <div className="row">
               {products &&
                 products.map((product, index) => (
-                  <div key={index} className="col-sm-12 col-md-6 col-lg-3 my-3">
-                    <div className="card p-3 rounded">
-                      <img
-                        className="card-img-top mx-auto"
-                        src={product.images[0].image}
-                      />
-                      <div className="card-body d-flex flex-column">
-                        <h5 className="card-title">
-                          <a href="">{product.name}</a>
-                        </h5>
-                        <div className="ratings mt-auto">
-                          <div className="rating-outer">
-                            <div
-                              className="rating-inner"
-                              style={{
-                                width: ` ${(product.ratings / 5) * 100}%`,
-                              }}
-                            ></div>
-                          </div>
-                          <span id="no_of_reviews">
-                            ({product.numOfReviews} Reviews)
-                          </span>
-                        </div>
-                        <p className="card-text">${product.price}</p>
-                        <a href="#" id="view_btn" className="btn btn-block">
-                          View Details
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+                  <Product product={product} index={index} />
                 ))}
             </div>
           </section>
