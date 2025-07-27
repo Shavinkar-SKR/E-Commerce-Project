435ab6fadd9f5ae308ba5742da791cd330d5384a
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-07-27 21:57:56 +0530

    Updated Products to use React Router Link
    Replaced anchor tags with React Router's Link component in Product.jsx for client-side navigation. Also fixed the logo image path in Header.jsx to use an absolute path.

--- frontend/src/components/product/Product.jsx
+++ frontend/src/components/product/Product.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Product({ product, index }) {
   return (
     <div key={index} className="col-sm-12 col-md-6 col-lg-3 my-3">
@@ -5,7 +7,7 @@ export default function Product({ product, index }) {
         <img className="card-img-top mx-auto" src={product.images[0].image} />
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">
-            <a href="">{product.name}</a>
+            <Link to={`/product/${product._id}`}>{product.name}</Link>
           </h5>
           <div className="ratings mt-auto">
             <div className="rating-outer">
@@ -19,9 +21,13 @@ export default function Product({ product, index }) {
             <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
           </div>
           <p className="card-text">${product.price}</p>
-          <a href="#" id="view_btn" className="btn btn-block">
+          <Link
+            to={`/product/${product._id}`}
+            id="view_btn"
+            className="btn btn-block"
+          >
             View Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
